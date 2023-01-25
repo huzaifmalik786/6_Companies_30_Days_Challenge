177b7c4e2cfaec6de14a95031de7fc3c167a965f
Huzaif Malik <100015095+huzaifmalik786@users.noreply.github.com>
2023-01-26 01:33:38 +0530

    KDivisibleElementsSubsets

---
+++ Google/KDivisibleElementsSubsets.cpp
@@ -0,0 +1,31 @@
+//link to solution: https://leetcode.com/problems/k-divisible-elements-subarrays/submissions/885237867/
+
+
+class Solution {
+public:
+
+    int countDistinct(vector<int>& nums, int k, int p) {
+        
+        int n=nums.size();
+        set<vector<int>>ans;
+        
+        int i,j;
+        for(i=0;i<n;i++)
+        {
+            vector<int>tt;
+            int ct=0;
+            for(j=i;j<n;j++)
+            {
+                tt.push_back(nums[j]);
+                if(nums[j]%p==0)
+                    ct++;
+                if(ct>k)
+                    break;
+                ans.insert(tt);
+                    
+            }
+        }
+        return ans.size();
+    }
+    
+};
